438e7f2f4e7b16bfdd7b4bb390d584ab6b6ceba7
codecampsdevcon-bukidnon <codecamps.devcon@gmail.com>
2026-07-30 16:51:38 +0800

    Refractor: test subject for changing name

--- portfolio_frontend/src/constants.ts
+++ portfolio_frontend/src/constants.ts
@@ -12,7 +12,7 @@ export const TESTNET_PACKAGE_ID = "0xb2eeb0e6b02570c6e4c386751800b4d1d8f6a851728
 // ✅ Your NEW Testnet Portfolio Object ID
 export const TESTNET_PORTFOLIO_ID = "0x0f607e3f9f07f7cefb451bfa8129a16b2ebf71438ee84336b198388871d81140";
 
-export const MAINNET_PACKAGE_ID = "0x";
+export const MAINNET_PACKAGE_ID = "0xa9432b109de897fde2ffc71a192e4ff857b1ef1d77ac5dbf1e4126ec6fb1e40b";
 export const MAINNET_PORTFOLIO_ID = "0x5cda36f91e4c22e9c68c14398e3a81362ed577b6bca9e0938b573375b931602a";
 
 export const LOCAL_PACKAGE_ID = "0x0";
